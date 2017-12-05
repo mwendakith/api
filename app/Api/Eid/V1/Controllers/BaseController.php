d97bcfa36c805890f099d0aed4d95e5968ca8d94
mwendakith <joelkith@gmail.com>
2017-12-05 12:14:04 +0300

    Error changes

--- app/Api/Eid/V1/Controllers/BaseController.php
+++ app/Api/Eid/V1/Controllers/BaseController.php
@@ -5,11 +5,14 @@ namespace App\Api\Eid\V1\Controllers;
 
 
 use App\Http\Controllers\Controller;
+use Dingo\Api\Routing\Helpers;
 use Illuminate\Http\Request;
 
 
 class BaseController extends Controller
 {
+	use Helpers;
+
 	protected $county_string = 'countys.ID as county_id, CountyDHISCode as CountyDHISCode, CountyMFLCode as CountyMFLCode, countys.name as County, ';
 
 	protected $subcounty_string = 'districts.ID as subcounty_id, districts.SubCountyDHISCode as SubCountyDHISCode, districts.SubCountyMFLCode as SubCountyMFLCode, districts.name as Subcounty, ';
@@ -23,35 +26,40 @@ class BaseController extends Controller
 	protected $patient_string = 'count(*) as `tests`, FacilityMFLcode, patientID';
 
 	protected function pass_error($message){
-    	return response()
-            ->json([
-                'error' => 500,
-                'message' =>  $message
-            ]);
+    	// return response()
+     //        ->json([
+     //            'error' => 500,
+     //            'message' =>  $message
+     //        ]);
+        return Response::make(['error' => $message], 400);
     }
 
     protected function invalid_month($month){
-		return response()
-            ->json([
-                'error' => 500,
-                'message' => 'Month ' . $month . ' is invalid. Value must be between 1 and 12.'
-            ]);
+		// return response()
+  //           ->json([
+  //               'error' => 500,
+  //               'message' => 'Month ' . $month . ' is invalid. Value must be between 1 and 12.'
+  //           ]);
+        return $this->pass_error('Month ' . $month . ' is invalid. Value must be between 1 and 12.');
 	}
 
 	protected function invalid_quarter($quarter){
-		return response()
-            ->json([
-                'error' => 500,
-                'message' => 'Quarter ' . $quarter . ' is invalid. Value must be between 1 and 4.'
-            ]);
+		// return response()
+  //           ->json([
+  //               'error' => 500,
+  //               'message' => 'Quarter ' . $quarter . ' is invalid. Value must be between 1 and 4.'
+  //           ]);
+        return $this->pass_error('Month ' . $month . ' is invalid. Value must be between 1 and 12.');
 	}
 
 	protected function invalid_type($type){
-		return response()
-            ->json([
-                'error' => 500,
-                'message' => 'Type ' . $type . ' is invalid. Value must be between 1 and 4. 1 is for the total for the whole year. 2 is for the year with data grouped by month. 3 is for the a particular month. 4 for a particular quarter.'
-            ]);
+		// return response()
+  //           ->json([
+  //               'error' => 500,
+  //               'message' => 'Type ' . $type . ' is invalid. Value must be between 1 and 4. 1 is for the total for the whole year. 2 is for the year with data grouped by month. 3 is for the a particular month. 4 for a particular quarter.'
+  //           ]);
+        $message = 'Type ' . $type . ' is invalid. Value must be between 1 and 4. 1 is for the total for the whole year. 2 is for the year with data grouped by month. 3 is for the a particular month. 4 for a particular quarter.';
+        return $this->pass_error($message);
 	}
 
 	protected function quarter_range($month){
